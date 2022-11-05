bdc1e1e6a478136daf940b5bd0b55eda7bf47204
Vlad Horban <ywvvvw@gmail.com>
2022-11-05 15:53:46 +0200

    feat: add pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+
+const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
+  const pageCount = Math.ceil(itemsCount / pageSize)
+  if (pageCount === 1) return null
+  const pages = _.range(1, pageCount + 1)
+
+  return (
+    <ul className="pagination">
+      {pages.map(page => (
+        <li
+          className={`page-item ${page === currentPage ? 'active' : ''}`}
+          key={`page-${page}`}
+        >
+          <button className="page-link" onClick={() => onPageChange(page)}>
+            {page}
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  currentPage: PropTypes.number.isRequired
+}
+
+export default Pagination
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,20 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import User from './User'
+import Pagination from './Pagination'
+import paginate from '../utils/paginate'
 
 const Users = ({ users, ...rest }) => {
+  const count = users.length
+  const pageSize = 4
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const handlePageChange = pageIndex => {
+    setCurrentPage(pageIndex)
+  }
+
+  const userCrop = paginate(users, currentPage, pageSize)
+
   return (
     <>
       {users.length > 0 && (
@@ -18,12 +31,18 @@ const Users = ({ users, ...rest }) => {
             </tr>
           </thead>
           <tbody>
-            {users.map(user => (
+            {userCrop.map(user => (
               <User key={user._id} {...rest} {...user} />
             ))}
           </tbody>
         </table>
       )}
+      <Pagination
+        itemsCount={count}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+        currentPage={currentPage}
+      />
     </>
   )
 }
